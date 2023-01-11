1699f7fc521e806a92a360e31c9e0d04c4906cf7
Ahmad Stanikzai <Shekibstanikzai.2016@gmail.com>
2023-01-11 11:30:37 -0800

    Done

---
+++ ORM-Dapper/DapperProductRepo.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    internal class DapperProductRepo : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+        //Constructor
+        public DapperProductRepo(IDbConnection connection)
+        { 
+            _connection = connection; 
+        }
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _connection.Execute("INSERT INTO product (Name, price, CategoryID) VALUES (@name, @price, @categoryID);",
+             new { name = name, price = price, categoryID = categoryID });
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM Departments;");
+        }
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                  .SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile("appsettings.json")
+                  .Build();
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+            #region Department
+            var departmentRepo = new DapperDepartment(conn);
+            departmentRepo.InsertDepartment("Fun Department");
+            var allDepartments = departmentRepo.GetAllDepartments();
+            foreach (Department item in allDepartments)
+            {
+                Console.WriteLine($"Department ID: {item.DepartmentID}");
+                Console.WriteLine($"Department Name: {item.Name}");
+            }
+
+            #endregion
+
+            #region product
+
+            var productRepo = new DapperProductRepo(conn);
+            productRepo.CreateProduct("IFun", 9.99, 10);
+            var allProduct = productRepo.GetAllProducts();
+
+            foreach (var item in allProduct)
+            {
+                Console.WriteLine($"{item.Name}");
+                Console.WriteLine($"{item.Price}");
+                Console.WriteLine($"{item.CategoryID}");
+            }
         }
+        #endregion
+
     }
+
 }
